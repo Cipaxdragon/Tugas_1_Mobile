e728f10987198dbefed74a7a3ecb473fa743be72
cipaxdragon <asep@gmail.com>
2023-11-21 01:14:37 +0800

    berhasil mi kalkulaktor ku aokwoa

---
+++ app/src/main/java/com/example/tugas_2/kalkulator.kt
@@ -0,0 +1,11 @@
+package com.example.tugas_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class kalkulator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kalkulator)
+    }
+}
