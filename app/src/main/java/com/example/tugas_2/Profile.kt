b43f966fd316bd2c9186bf7545d3a5b7a2f766ce
cipaxdragon <asep@gmail.com>
2023-11-21 12:25:06 +0800

    jadi mi profile

--- app/src/main/java/com/example/tugas_2/Profile.kt
+++ app/src/main/java/com/example/tugas_2/Profile.kt
@@ -1,11 +1,20 @@
 package com.example.tugas_2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val bek = findViewById<Button>(R.id.backButton)
+        bek.setOnClickListener{
+            val intent = Intent(this, kalkulator::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/tugas_2/kalkulator.kt
+++ app/src/main/java/com/example/tugas_2/kalkulator.kt
@@ -1,5 +1,6 @@
 package com.example.tugas_2
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
@@ -7,9 +8,14 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class kalkulator : AppCompatActivity() {
+    private lateinit var loginUsernameEditText: EditText
+    private lateinit var loginPasswordEditText: EditText
+
     private lateinit var etNumber1: EditText
     private lateinit var etNumber2: EditText
     private lateinit var tvResult: TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,19 @@ class kalkulator : AppCompatActivity() {
         etNumber1 = findViewById(R.id.etNumber1)
         etNumber2 = findViewById(R.id.etNumber2)
         tvResult = findViewById(R.id.tvResult)
+
+        // Menerima data dari aktivitas pendaftaran
+        val extras = intent.extras
+        if (extras != null) {
+            val registeredUsername = extras.getString("USERNAME")
+            val registeredPassword = extras.getString("PASSWORD")
+
+            // Set nilai ke EditText jika ada data yang dikirim
+            loginUsernameEditText.setText(registeredUsername)
+            loginPasswordEditText.setText(registeredPassword)
+        }
+
+
     }
 
     fun onAdd(view: View) {
@@ -43,9 +62,18 @@ class kalkulator : AppCompatActivity() {
         val num1 = etNumber1.text.toString().toInt()
         val num2 = etNumber2.text.toString().toInt()
         val result = num1 / num2
-        tvResult.text = "Result: $result"
+        tvResult.text = "Hasil: $result"
     }
+    fun onProfileClick(view: View) {
+        val newUsername = loginUsernameEditText.text.toString()
+        val newPassword = loginPasswordEditText.text.toString()
 
+        // Handle the click action, e.g., navigate to the profile activity
+        val intent = Intent(this, Profile::class.java)
+        intent.putExtra("User", newUsername)
+        intent.putExtra("Pass", newPassword)
+        startActivity(intent)
+    }
 
 }
 
